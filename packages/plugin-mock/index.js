cd2bf97f8b7c9bf1b408f9181901fc6a727db28e
JserWang <jserwang@gmail.com>
2021-05-07 22:25:02 +0800

    feat(plugin-mock): js env just watch `mock.json`

--- packages/plugin-mock/index.js
+++ packages/plugin-mock/index.js
@@ -1,26 +1,43 @@
 const path = require('path')
+const fs = require('fs')
 const { MockDataResolver } = require('ts-mock-generator')
-const { info } = require('@microprogram/shared-utils')
+const { info, readJsonFile } = require('@microprogram/shared-utils')
 
 let mockData = {}
 
+const cwd = process.cwd()
+
+function hasTsConfig() {
+  return fs.existsSync(path.join(cwd, 'tsconfig.json'))
+}
+
 const getOrGenerateMockData = (opts) => {
-  const mockDataResolver = new MockDataResolver({
-    configPath: path.join(process.cwd(), 'tsconfig.json'),
-    basePath: path.join(process.cwd(), opts.basePath),
-    mockDir: opts.mockDir ? path.join(process.cwd(), opts.mockDir) : undefined,
-    includes: opts.includes || []
-  })
-
-  mockData = mockDataResolver.getOrGenerateData()
-
-  if (opts.watchFile) {
-    mockDataResolver.watchMockFile((data) => {
-      mockData = data
+  //
+  if (hasTsConfig()) {
+    const mockDataResolver = new MockDataResolver({
+      configPath: path.join(cwd, 'tsconfig.json'),
+      basePath: path.join(cwd, opts.basePath),
+      mockDir: opts.mockDir ? path.join(cwd, opts.mockDir) : undefined,
+      includes: opts.includes || []
     })
-    mockDataResolver.watchRequestFile((data) => {
-      mockData = data
-    })
+  
+    mockData = mockDataResolver.getOrGenerateData()
+  
+    if (opts.watchFile) {
+      mockDataResolver.watchMockFile((data) => {
+        mockData = data
+      })
+      mockDataResolver.watchRequestFile((data) => {
+        mockData = data
+      })
+    }
+  } else {
+    const mockDir = path.join(cwd, opts.mockDir, 'mock.json')
+    if (fs.existsSync(mockDir)) {
+      require('node-watch')(mockDir, () => {
+        mockData = readJsonFile(mockDir) || []
+      })
+    }
   }
 }
 
@@ -38,9 +55,9 @@ module.exports = function (options) {
   return async (ctx, next) => {
     if (ctx.request.path.startsWith(prefix)) {
       const url = ctx.request.path.split('?')[0].slice(prefix.length)
-      const targetMockData = mockData.filter((data) => data.url === url)
-      if (targetMockData.length > 0) {
-        const data = targetMockData[0]
+      const targetMockData = mockData.find((data) => data.url === url)
+      if (targetMockData) {
+        const data = targetMockData
         if (data.timeout) {
           await sleep(data.timeout)
         }
