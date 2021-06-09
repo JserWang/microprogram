07815eb9018644382001754443415b96de606b10
JserWang <jserwang@gmail.com>
2021-06-09 17:39:29 +0800

    refactor(iconfont): just generate css file to target path

--- packages/plugin-iconfont/index.js
+++ packages/plugin-iconfont/index.js
@@ -1,10 +1,31 @@
-const { fetchSymbol } = require('./lib/fetchSymbol')
-const { resolveConfig } = require('@microprogram/shared-utils')
+const axios = require('axios')
+const fs = require('fs')
+const path = require('path')
+const { info, error, done, resolveConfig, runNpmScript } = require('@microprogram/shared-utils')
 
-const { platform, iconfont = {} } = resolveConfig()
+const cwd = process.cwd()
 
-module.exports = function () {
-  fetchSymbol(iconfont.url).then((res) => {
-    require(`./generator/${platform}`)(res, iconfont)
-  })
+module.exports = async function () {
+  const { iconfont } = resolveConfig()
+  const { url, dir, lint } = iconfont
+  info(`Starting fetch data from "${url}"`)
+  try {
+    const response = await axios.get(url)
+    const matches = String(response.data).match(/@font-face/)
+    if (!matches) {
+      throw new Error(`Unknown css url: "${url}"`)
+    }
+
+    const filePath = path.resolve(cwd, dir)
+    fs.writeFileSync(filePath, response.data)
+    done(`Write the data to "${filePath}"`)
+    if (lint) {
+      info('Start lint css')
+      await runNpmScript('stylelint', [dir, '--fix'])
+      done('Linted css')
+    }
+  } catch (err) {
+    error(err.message)
+    process.exit(1)
+  }
 }
--- packages/plugin-iconfont/lib/getTemplate.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-
-exports.getTemplate = function (fileName) {
-  return fs
-    .readFileSync(path.join(__dirname, `../templates/${fileName}.template`))
-    .toString()
-}
