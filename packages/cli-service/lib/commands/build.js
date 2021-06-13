800f3993529835d20c4bbd1e48dc8d0f3f007808
JserWang <jserwang@gmail.com>
2021-06-13 21:50:52 +0800

    feat(cli-service): inject mode into NODE_ENV when build

--- packages/cli-service/lib/commands/build.js
+++ packages/cli-service/lib/commands/build.js
@@ -1,3 +1,5 @@
+const argv = require('minimist')(process.argv.slice(2))
+
 module.exports = (api) => {
   api.registerCommand(
     'build',
@@ -8,7 +10,8 @@ module.exports = (api) => {
         '--no-clean': `do not remove the dist directory before building the project`
       }
     },
-    async function serve(args) {
+    async function serve() {
+      process.env.NODE_ENV = argv.mode
       const task = require('../task')
       task.execute(['build'])
     }
